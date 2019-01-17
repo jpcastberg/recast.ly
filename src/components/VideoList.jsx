84c26222ddb11c32033ccb46555a57560c08d82f
jpcastberg <jpcastberg@gmail.com>
2019-01-17 14:10:01 -0800

    Enable basic view

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,12 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
   </div>
 );
 
@@ -17,3 +19,43 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+// title: 'React JS Tutorial for Beginners - 1 - Introduction',
+// description: 'My website - https://www.thenewboston.com/videos.php Have questions about the tutorial or React? Ask them here ...',
+// url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/default.jpg',
+
+////////////////////////////////////////////////////////
+
+// {
+//   kind: 'youtube#searchResult',
+//   etag: 'abQHWywil_AkNqdqji7_FqiK-u4/Ykxo_CqKu8F8kcm-iNgL332gQTY',
+//   id: {
+//     kind: 'youtube#video',
+//     videoId: '4ZAEBxGipoA'
+//   },
+//   snippet: {
+//     publishedAt: '2015-08-02T20:52:58.000Z',
+//     channelId: 'UCJbPGzawDH1njbqV-D5HqKw',
+//     title: 'React JS Tutorial for Beginners - 1 - Introduction',
+//     description: 'My website - https://www.thenewboston.com/videos.php Have questions about the tutorial or React? Ask them here ...',
+//     thumbnails: {
+//       default: {
+//         url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/default.jpg',
+//         width: 120,
+//         height: 90
+//       },
+//       medium: {
+//         url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/mqdefault.jpg',
+//         width: 320,
+//         height: 180
+//       },
+//       high: {
+//         url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/hqdefault.jpg',
+//         width: 480,
+//         height: 360
+//       }
+//     },
+//     channelTitle: 'thenewboston',
+//     liveBroadcastContent: 'none'
+//   }
+// }
